20e4b5a7e079a8d860a2f810b80d9b028209fa69
Edilson <nantes.junior@gmail.com>
2025-04-09 17:52:36 -0300

    Initial commit

---
+++ src/main/java/com/dilso/desafio_simplificado/DesafioSimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.dilso.desafio_simplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSimplificadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dilso/desafio_simplificado/DesafioSimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilso.desafio_simplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
